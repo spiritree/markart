622ae95b16f3d9d1ecd0c3baf0a3a83ffdce2fd2
spiritree <spiritreess@gmail.com>
2018-01-13 20:32:06 +0800

    util: add crypto and messageHandler

---
+++ app/utils/crypto.ts
@@ -0,0 +1,17 @@
+import * as crypto from "crypto";
+
+export default class Crypto {
+  /**
+   * md5加密密码
+   * @static
+   * @param {string} password 
+   * @returns {string} 
+   * @memberof Crypto
+   */
+  public static encrypt(password: string): string {
+    return crypto
+      .createHash("md5")
+      .update(password)
+      .digest("hex");
+  }
+}
