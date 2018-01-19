bc2bf2cdde93a751fbf407b06d3532c178e75ea8
spiritree <spiritreess@gmail.com>
2018-01-19 17:26:11 +0800

    chore(*): update router config

--- app/middleware/index.ts
+++ app/middleware/index.ts
@@ -5,4 +5,5 @@ export const middlewareList: Array<string> = [
   'logger',
   'initAdmin',
   'restc',
+  'interceptor'
 ];
--- app/route.ts
+++ app/route.ts
@@ -1,10 +1,20 @@
 import * as Router from "koa-router";
-import { AuthController, HomeController } from "./controller";
+import {
+  AuthController,
+  HomeController,
+  ArticleController,
+  TagController
+} from "./controller";
 import { bindRoutes } from "trafficlight";
 
 export default function(app: any) {
   const routerRoutes = new Router({ prefix: "/api" });
-  bindRoutes(routerRoutes, [HomeController, AuthController]);
+  bindRoutes(routerRoutes, [
+    HomeController,
+    AuthController,
+    ArticleController,
+    TagController
+  ]);
   app.use(routerRoutes.routes());
   app.use(routerRoutes.allowedMethods());
 }
