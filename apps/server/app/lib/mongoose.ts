f0e4fab7d664d5fa903d2f53352a23923f213d5b
spiritree <spiritreess@gmail.com>
2018-03-08 16:54:06 +0800

    fix: production ready

--- apps/server/app/lib/mongoose.ts
+++ apps/server/app/lib/mongoose.ts
@@ -1,19 +1,23 @@
-import * as config from "../../config";
-import * as mongoose from "mongoose";
-const mongoosePaginate = require("mongoose-paginate");
+import * as config from '../../config';
+import * as mongoose from 'mongoose';
+const mongoosePaginate = require('mongoose-paginate');
 
 class mongooseConnect {
   public static connect(): void {
-    require("mongoose").Promise = Promise;
+    require('mongoose').Promise = Promise;
 
-    mongoose.connect(config.MONGODB.uri, { useMongoClient: true });
-
-    mongoose.connection.on("error", error => {
-      console.log("数据库连接失败!", error);
+    mongoose.connect(config.MONGODB.uri, {
+      useMongoClient: true,
+      user: config.MONGODB.username,
+      pass: config.MONGODB.password
     });
 
-    mongoose.connection.once("open", () => {
-      console.log("数据库连接成功!");
+    mongoose.connection.on('error', error => {
+      console.log('数据库连接失败!', error);
+    });
+
+    mongoose.connection.once('open', () => {
+      console.log('数据库连接成功!');
     });
   }
   public static loadPlugin(): void {
